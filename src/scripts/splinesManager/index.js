3f13bdb6ef9876204d15c114e2f9dee9f3094271
rkdshe <rkdshe@gmail.com>
2016-10-14 15:03:56 +0300

    added curve joints

--- src/scripts/splinesManager/index.js
+++ src/scripts/splinesManager/index.js
@@ -1,10 +1,17 @@
 const THREE = require('three');
 
 const lineMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
-
+const sphereGeometry = new THREE.SphereGeometry(0.01, 0.01, 0.01);
+const sphereMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
 class SplinesManager {
     constructor(scene) {
         this.scene = scene;
+        this.jointsContainer = new THREE.Object3D();
+        this.curvesContainer = new THREE.Object3D();
+        this.jointsContainer.name = 'jointsContainer';
+        this.curvesContainer.name = 'curvesContainer';
+        this.scene.add(this.jointsContainer);
+        this.scene.add(this.curvesContainer);
         this.splines = [];
 
         document.addEventListener('createSpline', this.createSpline.bind(this));
@@ -21,7 +28,17 @@ class SplinesManager {
         const splineMesh = new THREE.Line(geometry, lineMaterial);
         splineMesh.rotation.x = Math.PI / 2;
         splineMesh.position.y = 0.005;
-        this.scene.add(splineMesh);
+        this.curvesContainer.add(splineMesh);
+
+        const splineJoints = new THREE.Object3D();
+        this.jointsContainer.add(splineJoints);
+        splineCoords.forEach(coords => {
+            const jointMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+            splineJoints.add(jointMesh);
+            jointMesh.position.x = coords.x;
+            jointMesh.position.y = 0.005;
+            jointMesh.position.z = coords.y;
+        });
     }
 }
 
