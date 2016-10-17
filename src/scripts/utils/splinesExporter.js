746cf7fbbace9b04a7c8e26be3b5fde5b1d8cef9
rkdshe <rkdshe@gmail.com>
2016-10-17 15:13:35 +0300

    append current date to filename

--- src/scripts/utils/splinesExporter.js
+++ src/scripts/utils/splinesExporter.js
@@ -23,21 +23,40 @@ class SplinesExporter {
         const curveData = {};
         curveData.segmentsAmount = curveUserData.segmentsAmount;
         curveData.points = [];
+
         if (!params.applyTransform) {
             for (let i = 0; i < curveUserData.curve.points.length; i++) {
                 const point = curveUserData.curve.points[i];
+                curveData.points.push([point.x, point.y]);
+            }
+        } else {
+            for (let i = 0; i < curveUserData.curve.points.length; i++) {
+                const point = this.transformPoint(
+                    curveUserData.curve.points[i],
+                    params.leftUpXCoord,
+                    params.leftUpYCoord,
+                    params.rightUpXCoord,
+                    params.rightUpYCoord
+                );
                 curveData.points.push([point.x, point.y]);
             }
         }
         return curveData;
     }
 
+    transformPoint(point, luX, luY, ruX, ruY) {
+        return point;
+    }
+
     writeDataToFile(data) {
         const dataToWrite = JSON.stringify(data, null, 4);
+        const curDate = new Date().toString();
+        const dateArr = curDate.split(' ');
+        const dateToAppend = dateArr[4] + '_' + dateArr[1] + dateArr[2] + '_' + dateArr[3];
 
         window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
         window.requestFileSystem(window.TEMPORARY, 1024 * 1024, (fs) => {
-            fs.root.getFile('curves.bin', {create: true}, (fileEntry) => {
+            fs.root.getFile('curves_' + dateToAppend + '.bin', {create: true}, (fileEntry) => {
                 fileEntry.createWriter((fileWriter) => {
                     const blob = new Blob([dataToWrite]);
                     fileWriter.addEventListener('writeend', () => {
